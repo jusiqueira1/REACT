a3c5d60e5acbcb1f0aa57fdb7f5dc5d449e80d49
Julia <veigagithubjulia@gmail.com>
2024-06-26 16:28:39 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import Mensagem from './componentes/Saudacao';
 
-function App() {
+
+function saudacao() {
+  let nomee = "Júlia";
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Nome">
+     <Mensagem
+        nomee={nomee}
+     />
     </div>
   );
 }
-
-export default App;
+export default saudacao;
---
+++ src/componentes/Saudacao.jsx
@@ -0,0 +1,9 @@
+import "../style.css";
+
+function Saudacao({nomee}) {
+    return (
+        <h2 className="oi">Olá, {nomee}!</h2>
+    )
+}
+
+export default Saudacao;
